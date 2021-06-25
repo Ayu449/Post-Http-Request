d15865dcd6a88613018b1b396fc07243685b3625
Ayush Gupta <67874887+Ayu449@users.noreply.github.com>
2021-06-25 11:10:06 +0530

    Add files via upload

---
+++ Http Post request and converting data to Json/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text.Json;
+
+
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var values = new Dictionary<string, string>
+{
+    { "name", "John Doe" },
+    { "occupation", "gardener" }
+};
+
+            var data = new FormUrlEncodedContent(values);
+
+            var url = "https://httpbin.org/post";
+             var client = new HttpClient();
+
+            HttpResponseMessage response = await client.PostAsync(url, data);
+
+            string result = response.Content.ReadAsStringAsync().Result;
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
